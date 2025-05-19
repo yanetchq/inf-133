f4cec1d1e11e3ecd348464d8735da7de51b4409f
yanetchq <168309581+yanetchq@users.noreply.github.com>
2025-05-19 14:01:48 -0400

    Update db.js

--- Practica3/db.js
+++ Practica3/db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
   host: 'localhost',
-  user: 'yanet',
+  user: 'root',
   password: '123456',
   database: 'tienda'
 });
